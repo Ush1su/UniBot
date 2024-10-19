db65e846d8abd21492fbd0ba1040d8f6b3d2b59c
Ivan Osipenko <98388626+Ush1su@users.noreply.github.com>
2024-10-19 20:19:12 +0300

    Update config.py
    Change of config

--- config.py
+++ config.py
@@ -1,3 +1,3 @@
-TOKEN = "7476837078:AAGYw84YBGfsXqLgafW0yNVjMyNvDcCTu2A"
-organization = 'org-qT3TXRA7xLWXCjgrgS9TjYam'
-api_key = "sk-proj-MJvO3mBuuYoq66Q1xDobT3BlbkFJ6rUrAGitwI1LTkbTkgCN"
+TOKEN = "YOUR TELEGRAM TOKEN"
+organization = 'OPENAI ORGANIZATION FOR API'
+api_key = "OPENAI KEY FOR API"
